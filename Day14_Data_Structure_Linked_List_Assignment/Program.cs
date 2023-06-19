e52c5c29b4f253d73d095dc1ef288c2c47a0be6a
Rohan <rohandute1@gmail.com>
2023-06-19 20:30:52 +0530

    [Rohan] Welcome message successfully added to linked list assignment

---
+++ Day14_Data_Structure_Linked_List_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Data_Structure_Linked_List_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to linked list assignment");
+            Console.ReadLine();
+        }
+    }
+}
