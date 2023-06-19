500abc3f215a2c42ea346eca2b5e176b14b5a96b
Rohan <rohandute1@gmail.com>
2023-06-19 20:37:06 +0530

    [Rohan] Simple linked list created successfully

---
+++ Day14_Data_Structure_Linked_List_Assignment/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Data_Structure_Linked_List_Assignment
+{
+    internal class LinkedList
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        public Node Head;
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node currentNode = Head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = newNode;
+            }
+        }
+
+        public void PrintList()
+        {
+            Node currentNode = Head;
+
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Data + " ");
+                currentNode = currentNode.Next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- Day14_Data_Structure_Linked_List_Assignment/Program.cs
+++ Day14_Data_Structure_Linked_List_Assignment/Program.cs
@@ -11,6 +11,15 @@ namespace Day14_Data_Structure_Linked_List_Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list assignment");
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.AddNode(56);
+            linkedList.AddNode(30);
+            linkedList.AddNode(70);
+
+            linkedList.PrintList();
+            Console.ReadLine();
+
             Console.ReadLine();
         }
     }
